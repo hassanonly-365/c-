c20ce3a206e5b639759cf97ebfd889e347c8a0bd
hassanonly-365 <hassanwaqar205@outlook.com>
2024-09-19 19:55:44 +0500

    updating0

---
+++ 01_Setting+testing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //    cout<<"Hello World!"<< endl;
+    //    cout<<"Hello C++"<< endl;
+    cout<<"Hello World!"<< "\n";
+    cout<<"Hello C++"<< "\n";
+
+    return 0;
+}
