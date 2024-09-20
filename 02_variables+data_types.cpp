5184104cbe7a2e377fba3626eb49972df0556e1b
hassanonly-365 <hassanwaqar205@outlook.com>
2024-09-20 10:40:50 +0500

    updating

---
+++ 02_variables+data_types.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int x; //Decleration
+    // x=6; //assignment 
+    // cout<<x;
+    
+    // int x=5; // delceration and assignment
+    // int y=7;
+    // int sum = x+y;
+
+    // cout<<x<<"\n";
+    // cout<<y<<"\n";
+    // cout<<sum<<"\n";
+
+
+    //Data types---------------------------
+
+    //int (whole numbers)
+    int age= 19;
+    int year =2024;
+
+    //double (numbers including decimal)
+    double price = 299.95;
+    double cgpa = 2.75;
+    double temprature = 104.98;
+
+
+    //char (single character)
+    char grade = 'A';
+    char initial = 'B';
+    char grade = '$';
+
+    //boolean (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+
+    //string (objects that represent a sequence of text)
+
+    string name = "Hassan";
+    string day = "Friday";
+    string country = "Germany";
+    string address = "ISB";
+
+
+    return 0;
+}
---
+++ 03_const.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Const keyword specifies that a variables value is constant.
+    //tells the compiler to prevent anything from modifying it (read-only)
+
+    // double pi = 3.1415;
+    // double radius = 12;
+    // double circumference =2*pi*radius;
+
+    // cout<<circumference<<"cm \n";
+
+
+
+    // double pi = 3.1415;
+    // pi=156;
+    // double radius = 12;
+    // double circumference =2*pi*radius;
+
+    // cout<<circumference<<"cm \n";
+
+
+    // const double pi = 3.1415;
+    // pi=125;
+    // const double radius = 12;
+    // const double circumference =2*pi*radius;
+
+    // cout<<circumference<<"cm \n";
+
+
+    return 0;
+}
